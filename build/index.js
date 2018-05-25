168c350c42e1e31234e0d4943261795d77d38c9a
Jonathan Dai <nightghost5078@gmail.com>
2018-05-25 18:52:01 +0800

    Update grid system

--- build/index.js
+++ build/index.js
@@ -16,33 +16,45 @@ const isImage = require("is-image");
 const shell = require("shelljs");
 const readdirSorted = require("readdir-sorted");
 const pkg = require('../package.json');
-const MERGE_MODES = ['FILES', 'DIR_ALL', 'DIR_SEP'];
+const MERGE_MODES = ['ALL', 'SEP'];
 const BASE_NAME = 'merged_image_';
 const BASE_TYPES = ['JPG', 'PNG'];
 program.version(pkg.version)
     .description('image-merge-dir: merge images provided into one or several ones')
-    .option(`-m, --mode <${MERGE_MODES.join('|')}>`, 'merge modes:\n' +
-    '\tFILES: merge all files(-f) into one file\n' +
-    '\tDIR_ALL: merge all files under dir(-d) into one file\n' +
-    '\tDIR_SEP: merge all files under dir(-d) into several(-n) files')
-    .option('-f, --files <items>', 'list of source image files path, e.g: /path/to/file1,/path/to/file2,/path/to/file3,...', (files) => {
+    .option('-f, --source_files <items>', 'list of source image files path, e.g: /path/to/file1,/path/to/file2,/path/to/file3,...', (files) => {
     return files.split(',');
 })
-    .option('-d, --dir <string>', 'source image files dir')
-    .option('-n, --num <number>', 'target files number shall be merged into', parseInt)
-    .option('-l, --locale <string>', 'locale by which file list read from dir sorted, default is en, see https://www.npmjs.com/package/readdir-sorted')
-    .option('-o, --output_dir <dir>', 'Output directory')
+    .option('-d, --source_dir <string>', 'source image files dir')
+    .option(`-m, --output_mode <${MERGE_MODES.join('|')}>`, 'merge modes:\n' +
+    '\tALL: merge all source files into one file\n' +
+    '\tSEP: merge all source files into several(-n) files')
+    .option('-n, --output_num <number>', 'how many files shall be merged into', parseInt)
+    .option('-o, --output_dir <dir>', 'output directory')
     .option('-N, --output_name <string>', `output basename, optional, default is ${BASE_NAME}`)
     .option(`-t, --output_type <${BASE_TYPES.join('|')}>`, `output file type, only ${BASE_TYPES.join('|')} supported, default is ${BASE_TYPES[0]} since much smaller`)
+    .option('-x, --output_grid_x_num <number>', 'output images into grid, this value controls the columns number', parseInt)
+    .option('-y, --output_grid_y_num <number>', 'output images into grid, this value controls the lines number', parseInt)
+    .option('-l, --locale <string>', 'locale by which file list read from dir sorted, default is en, see https://www.npmjs.com/package/readdir-sorted')
     .parse(process.argv);
-const ARGS_MODE = program.mode === undefined ? undefined : program.mode;
-let ARGS_FILES = program.files === undefined ? [] : program.files;
-const ARGS_DIR = program.dir === undefined ? undefined : program.dir;
-const ARGS_NUM = program.num === undefined ? undefined : program.num;
-const ARGS_LOCALE = program.locale === undefined ? 'en' : program.locale;
+let ARGS_SOURCE_FILES = program.source_files === undefined ? [] : program.source_files;
+const ARGS_SOURCE_DIR = program.source_dir === undefined ? undefined : program.source_dir;
+const ARGS_OUTPUT_MODE = program.output_mode === undefined ? undefined : program.output_mode;
+const ARGS_OUTPUT_NUM = program.output_num === undefined ? undefined : program.output_num;
 const ARGS_OUTPUT_DIR = program.output_dir === undefined ? undefined : program.output_dir;
-const ARGS_OUTPUT_NAME = program.output_name === undefined ? BASE_NAME : program.output_name;
+const ARGS_OUTPUT_NAME = !program.output_name ? BASE_NAME : program.output_name;
 const ARGS_OUTPUT_TYPE = program.output_type === undefined ? BASE_TYPES[0] : program.output_type;
+const ARGS_OUTPUT_GRID_X_NUM = program.output_grid_x_num === undefined ? undefined : program.output_grid_x_num;
+const ARGS_OUTPUT_GRID_Y_NUM = program.output_grid_y_num === undefined ? undefined : program.output_grid_y_num;
+const ARGS_LOCALE = program.locale === undefined ? 'en' : program.locale;
+let GRID_TYPE = ''; // comes from ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM => x1 | 4x | 2x3 ...
+/**
+ * 参数列表应该按输入和输出分开：
+ * 输入参数需要添加文件夹内，起始文件位置，文件数量，来自由组织源图片组
+ *
+ * 重新制作参数验证部分，修改后的输入参数的互斥就完全改变了
+ *
+ * 添加grid图片功能
+ */
 class ImageMergeDir {
     run() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -54,28 +66,21 @@ class ImageMergeDir {
     _validate() {
         return __awaiter(this, void 0, void 0, function* () {
             console.log('Merge validating ...');
-            if (!shell.which('convert')) {
-                console.log('Command "convert" not found, you need to install "ImageMagick" first.\nOSX e.g: brew install imagemagick --with-webp');
+            if (!shell.which('convert') || !shell.which('montage')) {
+                console.log('Command "convert | montage" not found, you need to install "ImageMagick" first.\nOSX e.g: brew install imagemagick --with-webp');
                 process.exit(1);
             }
-            if (!ARGS_MODE) {
-                console.log('-m is required!');
+            // validate ARGS_SOURCE_FILES && ARGS_SOURCE_DIR
+            if (ARGS_SOURCE_FILES.length === 0 && !ARGS_SOURCE_DIR) {
+                console.log('Source files required, please provide -f or -d option');
                 process.exit(1);
             }
-            if (MERGE_MODES.indexOf(ARGS_MODE) === -1) {
-                console.log(`Invalid merge mode, please use one of: ${MERGE_MODES.join('|')}!`);
+            else if (ARGS_SOURCE_FILES.length !== 0 && ARGS_SOURCE_DIR) {
+                console.log('Only one source could be provided, -f or -d');
                 process.exit(1);
             }
-            if (!ARGS_OUTPUT_DIR || !(yield LibFs.stat(ARGS_OUTPUT_DIR)).isDirectory()) {
-                console.log('-o is required, and must be directory!');
-                process.exit(1);
-            }
-            if (ARGS_MODE === 'FILES') {
-                if (!ARGS_FILES) {
-                    console.log('-f is required when using "FILES" mode!');
-                    process.exit(1);
-                }
-                ARGS_FILES = ARGS_FILES.map((path) => {
+            else if (ARGS_SOURCE_FILES.length !== 0) {
+                ARGS_SOURCE_FILES = ARGS_SOURCE_FILES.map((path) => {
                     if (this._isPathRelative(path)) {
                         return LibPath.resolve(path);
                     }
@@ -83,43 +88,76 @@ class ImageMergeDir {
                         return path;
                     }
                 });
-                for (let file of ARGS_FILES) {
+                for (let file of ARGS_SOURCE_FILES) {
                     if (!(yield this._validateImageFile(file))) {
                         console.log(`File ${file} is not image!`);
                         process.exit(1);
                     }
                 }
             }
-            if (ARGS_MODE === 'DIR_ALL' || ARGS_MODE === 'DIR_SEP') {
-                if (!ARGS_DIR) {
-                    console.log('-d is required when using "DIR_*" mode!');
-                    process.exit(1);
-                }
-                if (!(yield LibFs.stat(ARGS_DIR)).isDirectory()) {
-                    console.log(`${ARGS_DIR} is not directory!`);
-                    process.exit(1);
-                }
-                if (ARGS_MODE === 'ARGS_MODE' && !ARGS_NUM) {
-                    console.log('-n is required when using "DIR_SEP" mode!');
+            else if (ARGS_SOURCE_DIR) {
+                if (!(yield LibFs.stat(ARGS_SOURCE_DIR)).isDirectory()) {
+                    console.log(`${ARGS_SOURCE_DIR} is not directory!`);
                     process.exit(1);
                 }
             }
-            if (BASE_TYPES.indexOf(ARGS_OUTPUT_TYPE.toUpperCase()) === -1) {
-                console.log(`-t ${ARGS_OUTPUT_TYPE} is not supported, supported ones: ${BASE_TYPES.join('|')}`);
+            // validate ARGS_OUTPUT_MODE && ARGS_OUTPUT_NUM
+            if (!ARGS_OUTPUT_MODE) {
+                console.log('Output mode is required, please provide -m');
                 process.exit(1);
+            }
+            else if (ARGS_OUTPUT_MODE && MERGE_MODES.indexOf(ARGS_OUTPUT_MODE) === -1) {
+                console.log(`Invalid output mode, please use one of: ${MERGE_MODES.join('|')}!`);
+                process.exit(1);
+            }
+            else if (ARGS_OUTPUT_MODE === 'SEP' && !ARGS_OUTPUT_NUM) {
+                console.log('Output num is required when using "SEP" mode, please provide -n');
+                process.exit(1);
+            }
+            // validate ARGS_OUTPUT_DIR
+            if (!ARGS_OUTPUT_DIR || !(yield LibFs.stat(ARGS_OUTPUT_DIR)).isDirectory()) {
+                console.log('Output dir is required, and must be directory, please provide -o');
+                process.exit(1);
+            }
+            // validate ARGS_OUTPUT_TYPE
+            if (ARGS_OUTPUT_TYPE && BASE_TYPES.indexOf(ARGS_OUTPUT_TYPE.toUpperCase()) === -1) {
+                console.log(`Output type ${ARGS_OUTPUT_TYPE} is not supported, supported ones: ${BASE_TYPES.join('|')}`);
+                process.exit(1);
+            }
+            // validate ARGS_OUTPUT_GRID_?_NUM && build GRID_TYPE
+            if (ARGS_OUTPUT_GRID_X_NUM && !ARGS_OUTPUT_GRID_Y_NUM) {
+                GRID_TYPE = `${ARGS_OUTPUT_GRID_X_NUM}x`;
+            }
+            else if (!ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM) {
+                GRID_TYPE = `x${ARGS_OUTPUT_GRID_Y_NUM}`;
+            }
+            else if (ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM) {
+                GRID_TYPE = `${ARGS_OUTPUT_GRID_X_NUM}x${ARGS_OUTPUT_GRID_Y_NUM}`;
             }
         });
     }
     _process() {
         return __awaiter(this, void 0, void 0, function* () {
-            switch (ARGS_MODE) {
-                case 'FILES':
-                    yield this._processFiles();
-                    break;
-                case 'DIR_ALL':
+            if (ARGS_SOURCE_FILES.length !== 0) {
+                yield this._processFiles();
+            }
+            else {
+                yield this._processDir();
+            }
+        });
+    }
+    _processFiles() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this._mergeFiles(ARGS_SOURCE_FILES.map(f => `"${f}"`), this._genOutputFilePath(0));
+        });
+    }
+    _processDir() {
+        return __awaiter(this, void 0, void 0, function* () {
+            switch (ARGS_OUTPUT_MODE) {
+                case MERGE_MODES[0]: // ALL
                     yield this._processDirAll();
                     break;
-                case 'DIR_SEP':
+                case MERGE_MODES[1]: // SEP
                     yield this._processDirSep();
                     break;
                 default:
@@ -127,20 +165,15 @@ class ImageMergeDir {
             }
         });
     }
-    _processFiles() {
-        return __awaiter(this, void 0, void 0, function* () {
-            this._mergeFiles(ARGS_FILES.map(f => `"${f}"`), this._genOutputFilePath(0));
-        });
-    }
     _processDirAll() {
         return __awaiter(this, void 0, void 0, function* () {
-            const dirFiles = yield readdirSorted(ARGS_DIR, {
+            const dirFiles = yield readdirSorted(ARGS_SOURCE_DIR, {
                 locale: ARGS_LOCALE,
                 numeric: true
             });
             let mergeTargets = [];
             for (const file of dirFiles) {
-                const fullPath = LibPath.join(ARGS_DIR, file);
+                const fullPath = LibPath.join(ARGS_SOURCE_DIR, file);
                 if (yield this._validateImageFile(fullPath)) {
                     mergeTargets.push(fullPath);
                 }
@@ -150,18 +183,18 @@ class ImageMergeDir {
     }
     _processDirSep() {
         return __awaiter(this, void 0, void 0, function* () {
-            const dirFiles = yield readdirSorted(ARGS_DIR, {
+            const dirFiles = yield readdirSorted(ARGS_SOURCE_DIR, {
                 locale: ARGS_LOCALE,
                 numeric: true
             });
             let mergeTargets = [];
             for (const file of dirFiles) {
-                const fullPath = LibPath.join(ARGS_DIR, file);
+                const fullPath = LibPath.join(ARGS_SOURCE_DIR, file);
                 if (yield this._validateImageFile(fullPath)) {
                     mergeTargets.push(fullPath);
                 }
             }
-            const dividedTargets = this._divideArrayIntoPiece(mergeTargets, ARGS_NUM);
+            const dividedTargets = this._divideArrayIntoPiece(mergeTargets, ARGS_OUTPUT_NUM);
             let index = 0;
             for (const targets of dividedTargets) {
                 this._mergeFiles(targets.map(f => `"${f}"`), this._genOutputFilePath(index));
@@ -170,7 +203,13 @@ class ImageMergeDir {
         });
     }
     _mergeFiles(files, outputFile) {
-        const command = `convert -append ${files.join(' ')} ${outputFile}`;
+        let command = '';
+        if (GRID_TYPE !== '') {
+            command = `montage -tile ${GRID_TYPE} -geometry +2+2 ${files.join(' ')} ${outputFile}`;
+        }
+        else {
+            command = `convert -append ${files.join(' ')} ${outputFile}`;
+        }
         console.log(`Merge images with command: ${command}`);
         const code = shell.exec(command).code;
         if (code !== 0) {
@@ -193,12 +232,17 @@ class ImageMergeDir {
         return path.charAt(0) !== '/';
     }
     _divideArrayIntoPiece(arr, pieceCount) {
-        const divided = [];
-        const size = Math.ceil(arr.length / pieceCount);
-        while (arr.length > 0) {
-            divided.push(arr.splice(0, size));
+        const listlen = arr.length;
+        const partlen = Math.floor(listlen / pieceCount);
+        const partrem = listlen % pieceCount;
+        let partition = [];
+        let mark = 0;
+        for (let px = 0; px < pieceCount; px++) {
+            let incr = (px < partrem) ? partlen + 1 : partlen;
+            partition[px] = arr.slice(mark, mark + incr);
+            mark += incr;
         }
-        return divided;
+        return partition;
     }
 }
 new ImageMergeDir().run().then(_ => _).catch(_ => console.log(_));
--- src/index.ts
+++ src/index.ts
@@ -10,38 +10,52 @@ import * as readdirSorted from 'readdir-sorted';
 
 const pkg = require('../package.json');
 
-const MERGE_MODES = ['FILES', 'DIR_ALL', 'DIR_SEP'];
+const MERGE_MODES = ['ALL', 'SEP'];
 const BASE_NAME = 'merged_image_';
 const BASE_TYPES = ['JPG', 'PNG'];
 
 program.version(pkg.version)
     .description('image-merge-dir: merge images provided into one or several ones')
-    .option(
-        `-m, --mode <${MERGE_MODES.join('|')}>`,
-        'merge modes:\n' +
-        '\tFILES: merge all files(-f) into one file\n' +
-        '\tDIR_ALL: merge all files under dir(-d) into one file\n' +
-        '\tDIR_SEP: merge all files under dir(-d) into several(-n) files'
-    )
-    .option('-f, --files <items>', 'list of source image files path, e.g: /path/to/file1,/path/to/file2,/path/to/file3,...', (files) => {
+    .option('-f, --source_files <items>', 'list of source image files path, e.g: /path/to/file1,/path/to/file2,/path/to/file3,...', (files) => {
         return files.split(',');
     })
-    .option('-d, --dir <string>', 'source image files dir')
-    .option('-n, --num <number>', 'target files number shall be merged into', parseInt)
-    .option('-l, --locale <string>', 'locale by which file list read from dir sorted, default is en, see https://www.npmjs.com/package/readdir-sorted')
-    .option('-o, --output_dir <dir>', 'Output directory')
+    .option('-d, --source_dir <string>', 'source image files dir')
+    .option(
+        `-m, --output_mode <${MERGE_MODES.join('|')}>`,
+        'merge modes:\n' +
+        '\tALL: merge all source files into one file\n' +
+        '\tSEP: merge all source files into several(-n) files'
+    )
+    .option('-n, --output_num <number>', 'how many files shall be merged into', parseInt)
+    .option('-o, --output_dir <dir>', 'output directory')
     .option('-N, --output_name <string>', `output basename, optional, default is ${BASE_NAME}`)
     .option(`-t, --output_type <${BASE_TYPES.join('|')}>`, `output file type, only ${BASE_TYPES.join('|')} supported, default is ${BASE_TYPES[0]} since much smaller`)
+    .option('-x, --output_grid_x_num <number>', 'output images into grid, this value controls the columns number', parseInt)
+    .option('-y, --output_grid_y_num <number>', 'output images into grid, this value controls the lines number', parseInt)
+    .option('-l, --locale <string>', 'locale by which file list read from dir sorted, default is en, see https://www.npmjs.com/package/readdir-sorted')
     .parse(process.argv);
 
-const ARGS_MODE = (program as any).mode === undefined ? undefined : (program as any).mode;
-let ARGS_FILES = (program as any).files === undefined ? [] : (program as any).files;
-const ARGS_DIR = (program as any).dir === undefined ? undefined : (program as any).dir;
-const ARGS_NUM = (program as any).num === undefined ? undefined : (program as any).num;
-const ARGS_LOCALE = (program as any).locale === undefined ? 'en' : (program as any).locale;
+let ARGS_SOURCE_FILES = (program as any).source_files === undefined ? [] : (program as any).source_files;
+const ARGS_SOURCE_DIR = (program as any).source_dir === undefined ? undefined : (program as any).source_dir;
+const ARGS_OUTPUT_MODE = (program as any).output_mode === undefined ? undefined : (program as any).output_mode;
+const ARGS_OUTPUT_NUM = (program as any).output_num === undefined ? undefined : (program as any).output_num;
 const ARGS_OUTPUT_DIR = (program as any).output_dir === undefined ? undefined : (program as any).output_dir;
-const ARGS_OUTPUT_NAME = (program as any).output_name === undefined ? BASE_NAME : (program as any).output_name;
+const ARGS_OUTPUT_NAME = !(program as any).output_name ? BASE_NAME : (program as any).output_name;
 const ARGS_OUTPUT_TYPE = (program as any).output_type === undefined ? BASE_TYPES[0] : (program as any).output_type;
+const ARGS_OUTPUT_GRID_X_NUM = (program as any).output_grid_x_num === undefined ? undefined : (program as any).output_grid_x_num;
+const ARGS_OUTPUT_GRID_Y_NUM = (program as any).output_grid_y_num === undefined ? undefined : (program as any).output_grid_y_num;
+const ARGS_LOCALE = (program as any).locale === undefined ? 'en' : (program as any).locale;
+
+let GRID_TYPE = ''; // comes from ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM => x1 | 4x | 2x3 ...
+
+/**
+ * 参数列表应该按输入和输出分开：
+ * 输入参数需要添加文件夹内，起始文件位置，文件数量，来自由组织源图片组
+ *
+ * 重新制作参数验证部分，修改后的输入参数的互斥就完全改变了
+ *
+ * 添加grid图片功能
+ */
 
 class ImageMergeDir {
 
@@ -55,75 +69,91 @@ class ImageMergeDir {
     private async _validate() {
         console.log('Merge validating ...');
 
-        if (!shell.which('convert')) {
-            console.log('Command "convert" not found, you need to install "ImageMagick" first.\nOSX e.g: brew install imagemagick --with-webp');
+        if (!shell.which('convert') || !shell.which('montage')) {
+            console.log('Command "convert | montage" not found, you need to install "ImageMagick" first.\nOSX e.g: brew install imagemagick --with-webp');
             process.exit(1);
         }
 
-        if (!ARGS_MODE) {
-            console.log('-m is required!');
+        // validate ARGS_SOURCE_FILES && ARGS_SOURCE_DIR
+        if (ARGS_SOURCE_FILES.length === 0 && !ARGS_SOURCE_DIR) {
+            console.log('Source files required, please provide -f or -d option');
             process.exit(1);
-        }
-        if (MERGE_MODES.indexOf(ARGS_MODE) === -1) {
-            console.log(`Invalid merge mode, please use one of: ${MERGE_MODES.join('|')}!`);
+        } else if (ARGS_SOURCE_FILES.length !== 0 && ARGS_SOURCE_DIR) {
+            console.log('Only one source could be provided, -f or -d');
             process.exit(1);
-        }
-
-        if (!ARGS_OUTPUT_DIR || !(await LibFs.stat(ARGS_OUTPUT_DIR)).isDirectory()) {
-            console.log('-o is required, and must be directory!');
-            process.exit(1);
-        }
-
-        if (ARGS_MODE === 'FILES') {
-            if (!ARGS_FILES) {
-                console.log('-f is required when using "FILES" mode!');
-                process.exit(1);
-            }
-            ARGS_FILES = ARGS_FILES.map((path: string) => {
+        } else if (ARGS_SOURCE_FILES.length !== 0) {
+            ARGS_SOURCE_FILES = ARGS_SOURCE_FILES.map((path: string) => {
                 if (this._isPathRelative(path)) {
                     return LibPath.resolve(path);
                 } else {
                     return path;
                 }
             });
-            for (let file of ARGS_FILES) {
+            for (let file of ARGS_SOURCE_FILES) {
                 if (!(await this._validateImageFile(file))) {
                     console.log(`File ${file} is not image!`);
                     process.exit(1);
                 }
             }
-        }
-
-        if (ARGS_MODE === 'DIR_ALL' || ARGS_MODE === 'DIR_SEP') {
-            if (!ARGS_DIR) {
-                console.log('-d is required when using "DIR_*" mode!');
-                process.exit(1);
-            }
-            if (!(await LibFs.stat(ARGS_DIR)).isDirectory()) {
-                console.log(`${ARGS_DIR} is not directory!`);
-                process.exit(1);
-            }
-            if (ARGS_MODE === 'ARGS_MODE' && !ARGS_NUM) {
-                console.log('-n is required when using "DIR_SEP" mode!');
+        } else if (ARGS_SOURCE_DIR) {
+            if (!(await LibFs.stat(ARGS_SOURCE_DIR)).isDirectory()) {
+                console.log(`${ARGS_SOURCE_DIR} is not directory!`);
                 process.exit(1);
             }
         }
 
-        if (BASE_TYPES.indexOf(ARGS_OUTPUT_TYPE.toUpperCase()) === -1) {
-            console.log(`-t ${ARGS_OUTPUT_TYPE} is not supported, supported ones: ${BASE_TYPES.join('|')}`);
+        // validate ARGS_OUTPUT_MODE && ARGS_OUTPUT_NUM
+        if (!ARGS_OUTPUT_MODE) {
+            console.log('Output mode is required, please provide -m');
             process.exit(1);
+        } else if (ARGS_OUTPUT_MODE && MERGE_MODES.indexOf(ARGS_OUTPUT_MODE) === -1) {
+            console.log(`Invalid output mode, please use one of: ${MERGE_MODES.join('|')}!`);
+            process.exit(1);
+        } else if (ARGS_OUTPUT_MODE === 'SEP' && !ARGS_OUTPUT_NUM) {
+            console.log('Output num is required when using "SEP" mode, please provide -n');
+            process.exit(1);
+        }
+
+        // validate ARGS_OUTPUT_DIR
+        if (!ARGS_OUTPUT_DIR || !(await LibFs.stat(ARGS_OUTPUT_DIR)).isDirectory()) {
+            console.log('Output dir is required, and must be directory, please provide -o');
+            process.exit(1);
+        }
+
+        // validate ARGS_OUTPUT_TYPE
+        if (ARGS_OUTPUT_TYPE && BASE_TYPES.indexOf(ARGS_OUTPUT_TYPE.toUpperCase()) === -1) {
+            console.log(`Output type ${ARGS_OUTPUT_TYPE} is not supported, supported ones: ${BASE_TYPES.join('|')}`);
+            process.exit(1);
+        }
+
+        // validate ARGS_OUTPUT_GRID_?_NUM && build GRID_TYPE
+        if (ARGS_OUTPUT_GRID_X_NUM && !ARGS_OUTPUT_GRID_Y_NUM) {
+            GRID_TYPE = `${ARGS_OUTPUT_GRID_X_NUM}x`;
+        } else if (!ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM) {
+            GRID_TYPE = `x${ARGS_OUTPUT_GRID_Y_NUM}`;
+        } else if (ARGS_OUTPUT_GRID_X_NUM && ARGS_OUTPUT_GRID_Y_NUM) {
+            GRID_TYPE = `${ARGS_OUTPUT_GRID_X_NUM}x${ARGS_OUTPUT_GRID_Y_NUM}`;
         }
     }
 
     private async _process() {
-        switch (ARGS_MODE) {
-            case 'FILES':
-                await this._processFiles();
-                break;
-            case 'DIR_ALL':
+        if (ARGS_SOURCE_FILES.length !== 0) {
+            await this._processFiles();
+        } else {
+            await this._processDir();
+        }
+    }
+
+    private async _processFiles() {
+        this._mergeFiles(ARGS_SOURCE_FILES.map(f => `"${f}"`), this._genOutputFilePath(0));
+    }
+
+    private async _processDir() {
+        switch (ARGS_OUTPUT_MODE) {
+            case MERGE_MODES[0]: // ALL
                 await this._processDirAll();
                 break;
-            case 'DIR_SEP':
+            case MERGE_MODES[1]: // SEP
                 await this._processDirSep();
                 break;
             default:
@@ -131,19 +161,15 @@ class ImageMergeDir {
         }
     }
 
-    private async _processFiles() {
-        this._mergeFiles(ARGS_FILES.map(f => `"${f}"`), this._genOutputFilePath(0));
-    }
-
     private async _processDirAll() {
-        const dirFiles = await readdirSorted(ARGS_DIR, {
+        const dirFiles = await readdirSorted(ARGS_SOURCE_DIR, {
             locale: ARGS_LOCALE,
             numeric: true
         });
         let mergeTargets = [];
 
         for (const file of dirFiles) {
-            const fullPath = LibPath.join(ARGS_DIR, file);
+            const fullPath = LibPath.join(ARGS_SOURCE_DIR, file);
             if (await this._validateImageFile(fullPath)) {
                 mergeTargets.push(fullPath);
             }
@@ -153,20 +179,20 @@ class ImageMergeDir {
     }
 
     private async _processDirSep() {
-        const dirFiles = await readdirSorted(ARGS_DIR, {
+        const dirFiles = await readdirSorted(ARGS_SOURCE_DIR, {
             locale: ARGS_LOCALE,
             numeric: true
         });
         let mergeTargets = [];
 
         for (const file of dirFiles) {
-            const fullPath = LibPath.join(ARGS_DIR, file);
+            const fullPath = LibPath.join(ARGS_SOURCE_DIR, file);
             if (await this._validateImageFile(fullPath)) {
                 mergeTargets.push(fullPath);
             }
         }
 
-        const dividedTargets = this._divideArrayIntoPiece(mergeTargets, ARGS_NUM);
+        const dividedTargets = this._divideArrayIntoPiece(mergeTargets, ARGS_OUTPUT_NUM);
 
         let index = 0;
         for (const targets of dividedTargets) {
@@ -176,7 +202,13 @@ class ImageMergeDir {
     }
 
     private _mergeFiles(files: Array<string>, outputFile: string) {
-        const command = `convert -append ${files.join(' ')} ${outputFile}`;
+        let command = '';
+
+        if (GRID_TYPE !== '') {
+            command = `montage -tile ${GRID_TYPE} -geometry +2+2 ${files.join(' ')} ${outputFile}`;
+        } else {
+            command = `convert -append ${files.join(' ')} ${outputFile}`;
+        }
 
         console.log(`Merge images with command: ${command}`);
 
@@ -204,14 +236,18 @@ class ImageMergeDir {
     }
 
     private _divideArrayIntoPiece(arr: Array<string>, pieceCount: number) {
-        const divided = [];
-        const size = Math.ceil(arr.length / pieceCount);
+        const listlen = arr.length;
+        const partlen = Math.floor(listlen / pieceCount);
+        const partrem = listlen % pieceCount;
 
-        while (arr.length > 0) {
-            divided.push(arr.splice(0, size));
+        let partition = [];
+        let mark = 0;
+        for(let px = 0; px < pieceCount; px ++) {
+            let incr = (px < partrem) ? partlen + 1 : partlen;
+            partition[px] = arr.slice(mark, mark + incr);
+            mark += incr;
         }
-
-        return divided;
+        return partition;
     }
 
 }
